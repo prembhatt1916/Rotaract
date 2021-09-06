f359c8fd1c91c5d790025a316a0ce17f1be9eaa6
Prem Bhatt <samratpb7890@gmail.com>
2021-09-06 10:41:31 +0530

    setting

--- Rotaract/settings.py
+++ Rotaract/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-django_heroku.setttings(locals())
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
